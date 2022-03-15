58320ff5b006c153bba0b7dc25ebbad9f051fc28
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2022-03-15 16:11:01 -0400

    logo nav link

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import Viking from "./assets/viking_logo.png"
 
 
 
@@ -8,13 +9,18 @@ import { NavLink } from "react-router-dom"
 const Navs = () => {
   return(
     // nav bar with links to each section of site
-    <nav style={{listStyleType: "none", margin: "2rem", padding: "0", fontSize: "4rem", justifyContent: "space-around" }}>
+    
+    <nav style={{listStyleType: "none", margin: "2rem", padding: "0", fontSize: "4rem", justifyContent: "space-around", backgroundColor: "hsl(25, 0%, 86%)", height: "10rem" }}>
+    
     <ul>
+    <NavLink to="/about"><img className="clearfix" src= {Viking} alt="logo" style={{ height: "10rem", width: "8rem", float: "right", padding: "1rem"}}></img></NavLink>
       {/* home, about, and contact page use NavLink */}
-      <li style={{display: "inline"}}><NavLink to="/about">{`About|`}</NavLink></li>
-      <li style={{display: "inline"}}><NavLink to="/portfolio">{`Portfolio|`}</NavLink></li>
-      <li style={{display: "inline"}}><NavLink to="/contact">Contact</NavLink></li>
+      <li style={{display: "inline", margin: "1rem"}}><NavLink to="/about">{`|About|`}</NavLink></li>
+      <li style={{display: "inline", margin: "1rem"}}><NavLink to="/portfolio">{`|Portfolio|`}</NavLink></li>
+      <li style={{display: "inline", margin: "1rem"}}><NavLink to="/contact">{`|Contact|`}</NavLink></li>
     </ul>
+   
+    
   </nav>
 )}
 
